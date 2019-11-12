76b66dc09b48d0436c9b6bddd062eb307e495d36
Peter van Huisstede <pvanhuisstede@ziggo.nl>
2019-11-12 15:59:28 +0100

    Work on the introduction
    Added some new examples and quick-checks.

---
+++ peter/aux/integer_division.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int result, remainder;
+  result = 2 / 3;
+  remainder = 2 % 3;
+
+  printf("Result is %d\n", result);
+  printf("Remainder is %d\n", remainder);
+
+  return 0;
+}
